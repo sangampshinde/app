2cf0ad80572d516fc787d50b2e768e8cab1befa1
sangam shinde <sangampshinde@gmail.com>
2024-10-12 20:15:41 +0530

    ok

---
+++ src/app/testmodeule/testmodeule.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Test1Component } from './test1/test1.component';
+import { Test2Component } from './test2/test2.component';
+
+
+
+@NgModule({
+  declarations: [
+    Test1Component,
+    Test2Component
+  ],
+  imports: [
+    CommonModule
+  ],
+  exports:[
+    
+  ]
+})
+export class TestmodeuleModule { }
